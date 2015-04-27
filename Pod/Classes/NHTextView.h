aecbef65361653c161a956ea4f9eabdcb3dfc25c
Naithar <devias.naith@gmail.com>
2015-04-27 12:24:58 +0300

    removed tests. properties for finding links, hashtags and mentions.

--- Pod/Classes/NHTextView.h
+++ Pod/Classes/NHTextView.h
@@ -10,6 +10,14 @@
 
 @interface NHTextView : UITextView
 
+@property (nonatomic, assign) BOOL findLinks;
+@property (nonatomic, assign) BOOL findHastags;
+@property (nonatomic, assign) BOOL findMentions;
+
+@property (nonatomic, copy) NSDictionary *linkAttributes;
+@property (nonatomic, copy) NSDictionary *hastagAttributes;
+@property (nonatomic, copy) NSDictionary *mentionAttributes;
+
 @property (nonatomic, copy) NSString *placeholder;
 @property (nonatomic, strong) UIFont *placeholderFont;
 @property (nonatomic, strong) UIColor *placeholderColor;
